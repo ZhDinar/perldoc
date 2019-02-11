794b682b2fc7e802fad3b931c802f3d7cf9fd609
Dinar <zhdinar@gmail.com>
2019-02-11 15:10:28 +0300

    clean: del actions.sh

--- actions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# get perlfunc for perl 5.26.1
-wget https://perl5.git.perl.org/perl.git/blob_plain/714a461a2ab3017f19ea0f7bbb4934b2309d3aa8:/pod/perlfunc.pod
-
-# set remote github repository
-git remote add origin https://github.com/ZhDinar/perldoc.git
-git push -u origin master
