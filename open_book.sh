195b0b1adcbd8572b963c3d7165bbb746328afd8
Dinar <zhdinar@gmail.com>
2019-02-11 15:07:36 +0300

    open_book: path from .book_location

--- open_book.sh
+++ open_book.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/sh /usr/bin/xdg-open "/my_disk/distr-repos/projects/books/repos/books/IT/Программирование на Perl, 4-е издание - 2014.djvu" 2>/dev/null
+/bin/sh /usr/bin/xdg-open "$(cat .book_location)" 2>/dev/null
